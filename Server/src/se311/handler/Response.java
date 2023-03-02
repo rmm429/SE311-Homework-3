1fddd38d65d5f259649078005920a110126d4ab9
rickymangerie <ricky@drexel.edu>
2023-03-02 12:40:49 -0500

    Updated the Response object

--- Server/src/se311/handler/Response.java
+++ Server/src/se311/handler/Response.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 
 public class Response implements Serializable {
 
-    ArrayList<String> matchingLines;
-    int totalLines;
-    int foundLines;
+    private ArrayList<String> matchingLines;
+    private int totalLines;
+    private int foundLines;
 
     public Response() {
         matchingLines = new ArrayList<>();
@@ -21,30 +21,65 @@ public class Response implements Serializable {
         foundLines = _foundLines;
     }
 
+    /**
+     * Add a single line to the list of matching lines
+     *
+     * @param   line 	The line to be added
+     */
     public void addLine(String line) {
         matchingLines.add(line);
     }
 
+    /**
+     * Set the entire list of matching lines
+     *
+     * @param   _matchingLines 	A list of lines
+     */
     public void setMatchingLines(ArrayList<String> _matchingLines) {
         matchingLines = _matchingLines;
     }
 
+    /**
+     * Set the number of total lines from the KWIC output
+     *
+     * @param   _totalLines 	The number of KWIC lines
+     */
     public void setTotalLines(int _totalLines) {
         totalLines = _totalLines;
     }
 
+    /**
+     * Set the number of matching lines from the search output
+     *
+     * @param   _foundLines 	The number of matching lines
+     */
     public void setFoundLines(int _foundLines) {
         foundLines = _foundLines;
     }
 
+    /**
+     * Get the list of matching lines
+     *
+     * @return                 The list of matching lines
+     */
     public ArrayList<String> getMatchingLines() {
         return matchingLines;
     }
 
+    /**
+     * Get the number of total lines from the KWIC output
+     *
+     * @return                 The number of KWIC lines
+     */
     public int getTotalLines() {
         return totalLines;
     }
 
+    /**
+     * Get the number of matching lines from the search output
+     *
+     * @return                 The number of matching lines
+     */
     public int getFoundLines() {
         return foundLines;
     }
